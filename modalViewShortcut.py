df659db2db5d3bf1ddfdd40e9694f78343480478
Milad-Laly <68471307+Milad-Laly@users.noreply.github.com>
2021-04-12 12:01:52 +0100

    Addition of Database and model view
    This improves to modal view layout with additional detail showing the percentage.
    
    Also, this includes a database.py file to create a SQLite3 database with the relevant information.

--- modalViewShortcut.py
+++ modalViewShortcut.py
@@ -10,8 +10,10 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 from watsonNLU import watsonNLU
 
+import database
+
 # Install the Slack app and get xoxb- token in advance
-app = App(token="BOT TOKEN HERE")
+app = App(token="ENTER XOXB TOKEN")
 
 
 @app.shortcut("tone_analysis_action")
@@ -19,20 +21,38 @@ def open_modal(ack, shortcut, client, logger):
     # Acknowledge shortcut request
     ack()
     try:
-        # Call the views.open method using the WebClient passed to listeners
-        #Call variable into modal
-        testVariable = "This is a test"
-
         #Gets text value from input
-        #Pass variable into database.py
         event = shortcut["message"]
         textValue = event.get("text", None) or event["text"]
 
         #Calls watsonNLU for sentiment
-        watsonSentimentText = watsonNLU(textValue)
+        watsonAnalysis = watsonNLU(textValue)
 
-        watsonSentimentDocumentScore = watsonSentimentText["sentiment"]["document"]["score"]
-        watsonSentimentDocumentTone = watsonSentimentText["sentiment"]["document"]["label"]
+        database.main(event,watsonAnalysis)
+
+        #Sentiment Options
+        watsonSentimentDocumentScore = watsonAnalysis["sentiment"]["document"]["score"] 
+        watsonSentimentScoreRounded = "{:.2%}".format(watsonSentimentDocumentScore)
+
+        watsonSentimentDocumentTone = watsonAnalysis["sentiment"]["document"]["label"]
+
+        #Emotion Options
+        watsonEmotionSadnessScore = watsonAnalysis["emotion"]["document"]["emotion"]["sadness"]
+        watsonEmotionSadnessRounded = "{:.2%}".format(watsonEmotionSadnessScore)
+
+        watsonEmotionJoyScore = watsonAnalysis["emotion"]["document"]["emotion"]["joy"]
+        watsonEmotionJoyRounded = "{:.2%}".format(watsonEmotionJoyScore)
+
+        watsonEmotionFearScore = watsonAnalysis["emotion"]["document"]["emotion"]["fear"]
+        watsonEmotionFearRounded = "{:.2%}".format(watsonEmotionFearScore)
+        
+        watsonEmotionDisgustScore = watsonAnalysis["emotion"]["document"]["emotion"]["disgust"]
+        watsonEmotionDisgustRounded = "{:.2%}".format(watsonEmotionDisgustScore)
+        
+        watsonEmotionAngerScore = watsonAnalysis["emotion"]["document"]["emotion"]["anger"]
+        watsonEmotionAngerRounded = "{:.2%}".format(watsonEmotionAngerScore)
+
+
         
         result = client.views_open(
             trigger_id=shortcut["trigger_id"],
@@ -42,25 +62,74 @@ def open_modal(ack, shortcut, client, logger):
                 "close": {"type": "plain_text", "text": "Close"},
                 "blocks": [
                     {
+                        "type": "header",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Sentiment of Text"
+                        }
+                    },
+                    {
                         "type": "section",
                         "text": {
                             "type": "mrkdwn",
-                            "text": "The tone of this text is " + watsonSentimentDocumentTone + " and the score is: " + str(watsonSentimentDocumentScore*100)+"%",
+                            "text": "The tone of this text is *" + watsonSentimentDocumentTone + "* and the score is " + str(watsonSentimentScoreRounded)
+                        }
+                    }, 
+                    #Maybe add sentence to improve mood if negative
+                    {
+                        "type": "divider"
+                    },
+                    {
+			            "type": "header",
+			            "text": {
+				            "type": "plain_text",
+				            "text": "Emotion of Text"
+			            }
+		            },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text":  "The sadness score of the text is " + str(watsonEmotionSadnessRounded)
                         },
                     },
                     {
                         "type": "section",
                         "text": {
                             "type": "mrkdwn",
-                            "text":  "twoText" + testVariable + " OOOO" ,
+                            "text":  "The joyfulness score of the text is " + str(watsonEmotionJoyRounded)
                         },
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text":  "The fearfulness score of the text is " + str(watsonEmotionFearRounded)
+                        },
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text":  "The disgusted score of the text is " + str(watsonEmotionDisgustRounded)
+                        },
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text":  "The angerness score of the text is " + str(watsonEmotionAngerRounded)
+                        },
+                    },
+                    {
+                        "type": "divider"
                     },
                     {
                         "type": "context",
                         "elements": [
                             {
                                 "type": "mrkdwn",
-                                "text": json.dumps(watsonSentimentText, indent=2),
+                                "text": json.dumps(watsonAnalysis, indent=2)
                             }
                         ],
                     },
@@ -75,4 +144,4 @@ def open_modal(ack, shortcut, client, logger):
 if __name__ == "__main__":
     # export SLACK_APP_TOKEN=xapp.....
     # export SLACK_BOT_TOKEN=xoxb.....
-    SocketModeHandler(app, "APP TOKEN HERE").start()
+    SocketModeHandler(app, "ENTER XAPP TOKEN ").start()
